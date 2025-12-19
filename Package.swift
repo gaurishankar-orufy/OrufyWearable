f8342fead23cde7a3cec4885c9edeb5662345dd9
Gauri Shankar <gaurishankar@orufy.com>
2025-12-19 11:03:30 +0530

    Fix Added

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .binaryTarget(
             name: "OrufyWearable",
             url: "https://github.com/gaurishankar-orufy/OrufyWearable/releases/download/1.1.3/OrufyWearable.zip",
-            checksum: "f38509aba5e614ca648d87cb5b83b6c8c4788c1d692f7d7ecb1acf3defa9b0f7"
+            checksum: "d7a3a9b36dc548110e27212a72f1c8adb6e9c3a2f676f02003d1339d031418d2"
         )
     ]
 )
